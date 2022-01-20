aff6a75d87e33b201e0980bcf31631f3d74192bc
rui-min <minruiray@outlook.com>
2022-01-19 22:06:13 -0500

    make a doc

--- docs/exampleData.sql
+++ docs/exampleData.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `tracking` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tracking`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: tracking
@@ -75,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-17 21:37:31
+-- Dump completed on 2022-01-19 21:58:28
